86debcc2cab323c08b947132cc9b53f70fe49f30
bidishasamantakgp <samantatukai@gmail.com>
2018-01-02 15:46:36 +0530

    Adding maxent classifier

--- CRF/language_tagging.py
+++ CRF/language_tagging.py
@@ -5,7 +5,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.metrics import classification_report
 import numpy as np 
+import random
 
+import nltk.data
+from nltk.util import ngrams
+
+classifier = nltk.data.load("classifiers/hindi_english_Maxent.pickle")
+
+
+def get_freq_dict(filename):
+	ll = []
+	with open(filename) as f:
+		lines = f.readlines()
+		for line in lines:
+			ll.append(line.strip().lower())	
+	return ll
+	
 def get_idfdict(filename):
     freq_dict = defaultdict(int)
     with codecs.open(filename, encoding="utf-8") as f:
@@ -16,11 +31,13 @@ def get_idfdict(filename):
             freq_dict[word] += 1
     return freq_dict
     
-def preprocess(filename):
+def preprocess(filename, size=None):
     docs = []
     with open(filename, 'r') as f:
         sentences = f.read().split('\n\n')
-
+    if size:
+	print "Size passed"
+	sentences = random.sample(sentences, size)
     for sentence in sentences:
         lines = sentence.split('\n')
         text = []
@@ -30,13 +47,15 @@ def preprocess(filename):
             if len(tokens)!=2:
 		continue
 	    # Word Langid
-	    text.append((tokens[1], tokens[0]))
-            #text.append((tokens[0], tokens[1]))
+	    #text.append((tokens[1], tokens[0]))
+            text.append((tokens[0], tokens[1]))
         docs.append(text)
     return docs
 
-
-def freq(word, lang):
+def freq(word, lang_dict):
+    
+    if word in lang_dict:
+		return True
     return False
     #For the time being just pass False
     '''
@@ -52,25 +71,27 @@ def freq(word, lang):
             return False
     '''
 
-def word2features(doc,i):
+def word2features(doc,i, hindi_dict, en_dict):
     #print "Extracting features", i
     word = doc[i][0]
     #postag = doc[i][1]
-    d = enchant.Dict("en_US")
+    #d = enchant.Dict("en_US")
     # Common features for all words
+    ngs = [word] + list(ngrams(list(word), 2)) + list(ngrams(list(word), 3))
+    feats = dict([(ng, True) for ng in ngs])
     features = [
         'bias',
         'word.lower=' + word.lower(),
-        'word[-3:]=' + word[-3:],
-        'word[-2:]=' + word[-2:],
+        #'word[-3:]=' + word[-3:],
+        #'word[-2:]=' + word[-2:],
         'word.isupper=%s' % word.isupper(),
         'word.istitle=%s' % word.istitle(),
         'word.isdigit=%s' % word.isdigit(),
         'word.startwithdigit=%s' %word[0].isdigit(),
         'word.length=' + str(len(word)),
-        'word.alphabet=%s' % d.check(word),
-        'word.ismostfreqEN=%s' % freq(word,'en'),
-        'word.ismostfreqHI=%s' % freq(word,'hi'),
+	'word.alphabet=%s' % classifier.classify(feats),
+        'word.ismostfreqEN=%s' % freq(word,en_dict),
+        'word.ismostfreqHI=%s' % freq(word,hi_dict),
     ]
 
     # Features for words that are not
@@ -106,15 +127,15 @@ def word2features(doc,i):
     return features
 
 
-def extract_features(doc):
-    #print "Extracting fetaures"
-    return [word2features(doc, i) for i in range(len(doc))]
+def extract_features(doc, hi_dict, en_dict):
+    print "Extracting fetaures"
+    return [word2features(doc, i, hi_dict, en_dict) for i in range(len(doc))]
 
 # A function fo generating the list of labels for each document
 def get_labels(doc):
     return [label for (token, label) in doc]
 
-def train(X_train, Y_train):
+def train(X_train, Y_train, hparams):
 	trainer = pycrfsuite.Trainer(verbose=False)
 
 	# Submit training data to the trainer
@@ -138,7 +159,7 @@ def train(X_train, Y_train):
 	})
 	# Provide a file name as a parameter to the train function, such that
 	# the model will be saved to the file when training is finished
-	trainer.train('crf.model')
+	trainer.train(hparams.model)
 	
 
 def parseargument():
@@ -147,22 +168,67 @@ def parseargument():
                         help='file for the vocabulary')
     parser.add_argument('--data_file', type=str, default='data.txt',
                         help='file for data')
+    parser.add_argument('--en_file', type=str, default='data.txt',
+                        help='file for data')
+    parser.add_argument('--hi_file', type=str, default='data.txt',
+                        help='file for data')
+
+    parser.add_argument('--train_file', type=str, default='train.txt',
+                        help='file with training data/ synthetic data')
+    parser.add_argument('--test_file', type=str, default='test.txt',
+                        help='file for test data')
+    parser.add_argument('--output_file', type=str, default='output.txt',
+                        help='file for data')
+    parser.add_argument('--size', type=int, default=None,
+                        help='training size')
+
+    parser.add_argument('--padding', type=bool, default=False,
+                        help='padding with original file')
+    parser.add_argument('--padding_file', type=str, default='original.txt',
+                        help='training file for padding with the original test')
+
+
+    parser.add_argument('--model', type=str, default='train.model',
+                        help='name of the model to be stored')
+
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
 
     hparams = parseargument()
-    data = preprocess(hparams.data_file)
+    #data = preprocess(hparams.data_file)
+    hi_dict = get_freq_dict(hparams.en_file) 
+    en_dict = get_freq_dict(hparams.hi_file) 
     #print "Preprocess done", len(data[0])
-    X = [extract_features(doc) for doc in data]
-    Y = [get_labels(doc) for doc in data]
-    #print "Fetaure extraction done"
-    kf = KFold(n_splits=5)
+    train_data = preprocess(hparams.train_file, hparams.size)
+    #train_data = preprocess(hparams.train_file)
+    if hparams.padding :
+	train_data.extend(preprocess(hparams.padding_file, 200))
+    
+    X_train = [extract_features(doc, hi_dict, en_dict) for doc in train_data]
+    Y_train = [get_labels(doc) for doc in train_data]
+    
+    #hparams.size = 500
+    test_data = preprocess(hparams.test_file)
+    X_test = [extract_features(doc,hi_dict, en_dict) for doc in test_data]
+    Y_test = [get_labels(doc) for doc in test_data]
+    ''' 
+    X_train_new = X_train[:250]
+    Y_train_new = Y_train[:250]
+
+    X_test = X_train[250:]
+    Y_test  = Y_train[250:]
+
+    X_train = X_train_new
+    Y_train = Y_train_new
+    '''
+    #print "Fetaure extraction done", len(X_test), len(Y_test), len(X_train), len(Y_train)
+    '''
+    kf = KFold(n_splits=2)
     i = 0
     for train_index, test_index in kf.split(X):
 	#print train_index, test_index, X
-	
     	X_train = [] 
 	X_test= []
 	Y_train= []
@@ -173,30 +239,30 @@ if __name__ == "__main__":
 	for i in test_index:
                 X_test.append(X[i])
                 Y_test.append(Y[i])
-	#X[train_index], X[test_index], Y[train_index], Y[test_index]
-	#train_test_split(X, Y, test_size=0.2)
+     '''
+    #X[train_index], X[test_index], Y[train_index], Y[test_index]
+    #train_test_split(X, Y, test_size=0.2)
 
-    	train(X_train, Y_train)
-    	tagger = pycrfsuite.Tagger()
-    	tagger.open('crf.model') 
-    	y_pred = [tagger.tag(xseq) for xseq in X_test]
+    train(X_train, Y_train, hparams)
+    tagger = pycrfsuite.Tagger()
+    tagger.open(hparams.model) 
+    y_pred = [tagger.tag(xseq) for xseq in X_test]
 
-    	# Let's take a look at a random sample in the testing set
-    	'''
-	i = 12
-    	for i in range(len(X_test)):
+    labels = {"EN": 1, "HI": 0, "OTHER": 2, "NE": 3}
+    # Let's take a look at a random sample in the testing set
+    
+    i = 12
+    '''
+    for i in range(len(X_test)):
      		for x, y in zip(y_pred[i], [x[1].split("=")[1] for x in X_test[i]]):
     			print("%s (%s)" % (y, x))
-    	'''
-	# Create a mapping of labels to indices
-    	labels = {"EN": 1, "HI": 0, "OTHER": 2, "NE": 3}
-
-    	# Convert the sequences of tags into a 1-dimensional array
-    	predictions = np.array([labels[tag] for row in y_pred for tag in row])
-    	truths = np.array([labels[tag] for row in Y_test for tag in row])
-	i = i + 1
-	print "i=", i
-    	# Print out the classification report
-    	print(classification_report(
+    '''
+    # Convert the sequences of tags into a 1-dimensional array
+    predictions = np.array([labels[tag] for row in y_pred for tag in row])
+    truths = np.array([labels[tag] for row in Y_test for tag in row])
+    	
+    # Print out the classification report
+    with open(hparams.output_file, 'a') as f:	
+     	f.write(classification_report(
     	truths, predictions,
     	target_names=["HI", "EN", "OTHER", "NE"]))
